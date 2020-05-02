8418fbcd766a1770c13e64c7954547b2ed3ce44d
rijan <hrijon.chp@gmail.com>
2020-05-02 11:43:33 +0800

    request handler css

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ var start = require("./requestHandlers/start");
 var insert = require("./requestHandlers/insert");
 var view = require("./requestHandlers/view");
 var upload = require("./requestHandlers/upload");
-var imageFolder = require("./requestHandlers/imageFolder");
+var showRoom = require("./requestHandlers/showRoom");
+var styles = require("./requestHandlers/styles");
 
 // create ‘handle’ object literal
 var handle = {};
 // using the associative array notation, each array index is an
 // object property which points to an appropriate request handler
 
-handle["/css"] = start.reqCss;
+handle["/css/styles.css"] = styles.reqCss;
 handle["/"] = start.reqStart;
 handle["/start"] = start.reqStart;
 handle["/check"] = start.reqCheck;
@@ -21,14 +22,14 @@ handle["/insert"] = insert.reqInsert;
 handle["/import"] = insert.reqImport;
 
 handle["/view"] = view.reqView;
-handle["/selectDegree"] = view.reqSelectDegree;
+handle["/date"] = view.reqDate;
 handle["/display"] = view.reqDisplay;
 
 handle["/upload"] = upload.reqUpload;
 handle["/show"] = upload.reqShow;
 handle["/dispImage"] = upload.dispImage;
 
-handle["/images"] = imageFolder.reqImages;
+handle["/showRoom"] = showRoom.reqShowRoom;
 
 // ***JSON format***
 // handle = {
